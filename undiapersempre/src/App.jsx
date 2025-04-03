4e662f4ace6625e9b7aeceaa53b668884a3e208f
HikaruRyu <acalle@inscastellet.cat>
2025-04-03 11:22:11 +0200

    feat(CountdownPage): webs-20 made a countdown layout

--- undiapersempre/src/App.jsx
+++ undiapersempre/src/App.jsx
@@ -1,27 +1,32 @@
-import { Facebook, Instagram, Menu, Twitter, Youtube } from "lucide-react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/layout"
-import Home from "./pages/Home"
-import ConfirmAssistance from "./pages/ConfirmAssistance"
-import DayOfTheEvent from "./pages/DayOfTheEvent"
-import FAQs from "./pages/FAQs"
-import ScrollToTop from "./components/ScrollToTop"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import ConfirmAssistance from "./pages/ConfirmAssistance";
+import DayOfTheEvent from "./pages/DayOfTheEvent";
+import FAQs from "./pages/FAQs";
+import ScrollToTop from "./components/ScrollToTop";
+import Countdown from "./pages/Countdown";
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/confirm-assistance" element={<ConfirmAssistance />} />
-          <Route path="/day-of-the-event" element={<DayOfTheEvent />} />
-          <Route path="/faqs" element={<FAQs />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route 
+          path="/" 
+          element={import.meta.env.DEV ? <Countdown /> : <Layout><Home /></Layout> } 
+        />
+
+        {import.meta.env.DEV && (
+          <>
+            <Route path="/confirm-assistance" element={<Layout><ConfirmAssistance /></Layout>} />
+            <Route path="/day-of-the-event" element={<Layout><DayOfTheEvent /></Layout>} />
+            <Route path="/faqs" element={<Layout><FAQs /></Layout>} />
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
-
+export default App;
