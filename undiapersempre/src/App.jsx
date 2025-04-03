ffa20079efb6d687455075d6aa3077b4456252de
Lidia Miron <lidiamirion@gmail.com>
2025-04-03 18:32:59 +0200

    feat(valid): gbp-13 create validation page

--- undiapersempre/src/App.jsx
+++ undiapersempre/src/App.jsx
@@ -1,11 +1,12 @@
-import { Facebook, Instagram, Menu, Twitter, Youtube } from "lucide-react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Layout from "./components/layout"
-import Home from "./pages/Home"
-import ConfirmAssistance from "./pages/ConfirmAssistance"
-import DayOfTheEvent from "./pages/DayOfTheEvent"
-import FAQs from "./pages/FAQs"
-import ScrollToTop from "./components/ScrollToTop"
+import { Facebook, Instagram, Menu, Twitter, Youtube } from "lucide-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./pages/Home";
+import ConfirmAssistance from "./pages/ConfirmAssistance";
+import DayOfTheEvent from "./pages/DayOfTheEvent";
+import FAQs from "./pages/FAQs";
+import ScrollToTop from "./components/ScrollToTop";
+import CodeValidation from "./pages/validationCode";
 
 function App() {
   return (
@@ -17,11 +18,11 @@ function App() {
           <Route path="/confirm-assistance" element={<ConfirmAssistance />} />
           <Route path="/day-of-the-event" element={<DayOfTheEvent />} />
           <Route path="/faqs" element={<FAQs />} />
+          <Route path="/validation" element={<CodeValidation />} />
         </Routes>
       </Layout>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
-
+export default App;
