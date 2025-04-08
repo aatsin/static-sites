37419b000d038f69bdebf99b439cfd60fc0feb17
Patriciu Nista <patriciu.nista@gmail.com>
2025-04-08 16:29:11 +0200

    fix merge issues
    remove env

--- undiapersempre/src/App.jsx
+++ undiapersempre/src/App.jsx
@@ -1,4 +1,3 @@
-import { Facebook, Instagram, Menu, Twitter, Youtube } from "lucide-react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/Home";
@@ -6,21 +5,64 @@ import ConfirmAssistance from "./pages/ConfirmAssistance";
 import DayOfTheEvent from "./pages/DayOfTheEvent";
 import FAQs from "./pages/FAQs";
 import ScrollToTop from "./components/ScrollToTop";
-import CodeValidation from "./pages/validationCode";
+import CountdownPage from "./pages/CountdownPage";
+import UpcomingProduction from "./pages/UpcomingProduction";
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/confirm-assistance" element={<ConfirmAssistance />} />
-          <Route path="/day-of-the-event" element={<DayOfTheEvent />} />
-          <Route path="/faqs" element={<FAQs />} />
-          <Route path="/validation" element={<CodeValidation />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            import.meta.env.DEV ? (
+              <Layout>
+                <Home />
+              </Layout>
+            ) : (
+              <CountdownPage />
+            )
+          }
+        />
+
+        {import.meta.env.DEV && (
+          <>
+            <Route
+              path="/confirm-assistance"
+              element={
+                <Layout>
+                  <ConfirmAssistance />
+                </Layout>
+              }
+            />
+            <Route
+              path="/day-of-the-event"
+              element={
+                <Layout>
+                  <DayOfTheEvent />
+                </Layout>
+              }
+            />
+            <Route
+              path="/faqs"
+              element={
+                <Layout>
+                  <FAQs />
+                </Layout>
+              }
+            />
+            <Route
+              path="/upcoming-production"
+              element={
+                <Layout>
+                  <UpcomingProduction />
+                </Layout>
+              }
+            />
+          </>
+        )}
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ undiapersempre/src/pages/UpcomingProduction.jsx
@@ -0,0 +1,3 @@
+export default function UpcomingProduction() {
+  return <div>UpcomingProduction</div>;
+}
